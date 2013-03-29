113556daa18dc802da24ba1fc71a9666843cf311
Jordan Harband <jharband@twitter.com>
2013-03-29 14:29:36 -0500

    Adding tests.

---
+++ test.js
@@ -0,0 +1,31 @@
+var test = require('tap').test;
+var keys = require('./index.js').shim;
+
+test('works', function (t) {
+	var obj = {
+		a: true,
+		b: true,
+		c: true
+	};
+	var objKeys = ['a', 'b', 'c'];
+
+	t.test('working with actual shim', function (st) {
+		st.plan(1);
+		st.notEqual(Object.keys, keys, 'keys shim is not native Object.keys');
+	});
+
+	t.test('works with an object literal', function (st) {
+		st.plan(1);
+		st.deepEqual(keys(obj), objKeys, 'Object has expected keys');
+	});
+});
+
+test('works with an object instance', function (t) {
+	t.plan(1);
+	var Prototype = function () {};
+	Prototype.prototype.foo = true;
+	var obj = new Prototype();
+	obj.bar = true;
+	t.deepEqual(keys(obj), ['bar'], 'Instance has expected keys');
+});
+
