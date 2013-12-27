07277e91e57d2855d29786223f75dcc3c11c19f9
Jordan Harband <ljharb@gmail.com>
2013-12-26 23:43:05 -0800

    Adding an isArguments test for old-style arguments

--- test/isArguments.js
+++ test/isArguments.js
@@ -5,6 +5,11 @@ test('is.arguments', function (t) {
 	t.notOk(isArguments([]), 'array is not arguments');
 	(function () { t.ok(isArguments(arguments), 'arguments is arguments'); }());
 	(function () { t.notOk(isArguments(Array.prototype.slice.call(arguments)), 'sliced arguments is not arguments'); }());
+	var fakeOldArguments = {
+		length: 3,
+		callee: function () {}
+	};
+	t.ok(isArguments(fakeOldArguments), 'old-style arguments is arguments');
 	t.end();
 });
 
