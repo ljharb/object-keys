e3c1f16ad735e54b678adff1ee65e12f18c18fc5
Jordan Harband <ljharb@gmail.com>
2014-12-27 23:17:42 -0700

    Don't shadow the function name.

--- isArguments.js
+++ isArguments.js
@@ -4,14 +4,14 @@ var toStr = Function.call.bind(Object.prototype.toString);
 
 module.exports = function isArguments(value) {
 	var str = toStr(value);
-	var isArguments = str === '[object Arguments]';
-	if (!isArguments) {
-		isArguments = str !== '[object Array]'
+	var isArgs = str === '[object Arguments]';
+	if (!isArgs) {
+		isArgs = str !== '[object Array]'
 			&& value !== null
 			&& typeof value === 'object'
 			&& typeof value.length === 'number'
 			&& value.length >= 0
 			&& toStr(value.callee) === '[object Function]';
 	}
-	return isArguments;
+	return isArgs;
 };
