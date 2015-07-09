2087bca632223db92ba3af5aedc650538664d34a
Jordan Harband <ljharb@gmail.com>
2015-07-09 10:20:56 -0500

    Fix `no-shadow` rule, as well as an IE 8 bug caused by engine NFE shadowing bugs.

--- test/shim.js
+++ test/shim.js
@@ -2,7 +2,7 @@
 
 var test = require('tape');
 var is = require('is');
-var keys = require('../index.js');
+var keysShim = require('../index.js');
 var indexOf = require('indexof');
 var has = Object.prototype.hasOwnProperty;
 var enumerable = Object.prototype.propertyIsEnumerable;
@@ -31,13 +31,13 @@ var preserve = function preserve(object, property, callback) {
 };
 
 test('exports a "shim" function', function (t) {
-	t.equal(typeof keys.shim, 'function', 'keys.shim is a function');
+	t.equal(typeof keysShim.shim, 'function', 'keysShim.shim is a function');
 
 	t.test('when Object.keys is present', preserve(Object, 'keys', function (st) {
 		Object.keys = noop;
 		st.equal(Object.keys, noop, 'Object.keys has been replaced');
-		var shimmedKeys = keys.shim();
-		st.notEqual(Object.keys, keys, 'Object.keys is not overridden');
+		var shimmedKeys = keysShim.shim();
+		st.notEqual(Object.keys, keysShim, 'Object.keys is not overridden');
 		st.equal(shimmedKeys, Object.keys, 'Object.keys is returned');
 		st.end();
 	}));
@@ -46,9 +46,9 @@ test('exports a "shim" function', function (t) {
 		Object.keys = undefined;
 		delete Object.keys;
 		st.notOk(Object.keys, 'Object.keys has been deleted');
-		var shimmedKeys = keys.shim();
-		st.equal(Object.keys, keys, 'Object.keys is overridden');
-		st.equal(shimmedKeys, keys, 'shim is returned');
+		var shimmedKeys = keysShim.shim();
+		st.equal(Object.keys, keysShim, 'Object.keys is overridden');
+		st.equal(shimmedKeys, keysShim, 'shim is returned');
 		st.end();
 	}));
 
@@ -59,7 +59,7 @@ test('exports a "shim" function', function (t) {
 			return originalObjectKeys(object);
 		};
 		st.notDeepEqual(Object.keys(arguments), ['0'], 'Object.keys has arguments bug');
-		var shimmedKeys = keys.shim();
+		var shimmedKeys = keysShim.shim();
 		st.equal(Object.keys, shimmedKeys, 'Object.keys is overridden');
 		st.deepEqual(Object.keys(arguments), ['0'], 'Object.keys now works with arguments');
 		st.end();
@@ -69,12 +69,12 @@ test('exports a "shim" function', function (t) {
 });
 
 test('working with actual shim', function (t) {
-	t.notEqual(Object.keys, keys, 'keys shim is not native Object.keys');
+	t.notEqual(Object.keys, keysShim, 'keys shim is not native Object.keys');
 	t.end();
 });
 
 test('works with an object literal', function (t) {
-	var theKeys = keys(obj);
+	var theKeys = keysShim(obj);
 	t.equal(is.array(theKeys), true, 'returns an array');
 	t.deepEqual(theKeys, objKeys, 'Object has expected keys');
 	t.end();
@@ -83,27 +83,27 @@ test('works with an object literal', function (t) {
 test('works with an arguments object', function (t) {
 	(function () {
 		t.equal(arguments.length, 3, 'arguments has length of 3');
-		t.deepEqual(keys(arguments), ['0', '1', '2'], 'returns keys of arguments');
+		t.deepEqual(keysShim(arguments), ['0', '1', '2'], 'returns keys of arguments');
 	}(1, 2, 3));
 	t.end();
 });
 
 test('works with a boxed primitive', function (t) {
-	t.deepEqual(keys(Object('hello')), ['0', '1', '2', '3', '4'], 'object string returns proper keys');
+	t.deepEqual(keysShim(Object('hello')), ['0', '1', '2', '3', '4'], 'object string returns proper keys');
 	/* eslint-disable no-new-wrappers */
-	t.deepEqual(keys(new String('hello')), ['0', '1', '2', '3', '4'], 'String object returns proper keys');
+	t.deepEqual(keysShim(new String('hello')), ['0', '1', '2', '3', '4'], 'String object returns proper keys');
 
 	var x = new String('x');
 	/* eslint-enable no-new-wrappers */
 	x.y = 1;
-	t.deepEqual(keys(x).sort(), ['0', 'y'].sort(), 'String object with extra properties returns proper keys');
+	t.deepEqual(keysShim(x).sort(), ['0', 'y'].sort(), 'String object with extra properties returns proper keys');
 
 	t.end();
 });
 
 test('works with an array', function (t) {
 	var arr = [1, 2, 3];
-	var theKeys = keys(arr);
+	var theKeys = keysShim(arr);
 	t.equal(is.array(theKeys), true, 'returns an array');
 	t.deepEqual(theKeys, ['0', '1', '2'], 'Array has expected keys');
 	t.end();
@@ -113,13 +113,13 @@ test('works with a function', function (t) {
 	var foo = function () {};
 	foo.a = true;
 
-	t.doesNotThrow(function () { return keys(foo); }, 'does not throw an error');
-	t.deepEqual(keys(foo), ['a'], 'returns expected keys');
+	t.doesNotThrow(function () { return keysShim(foo); }, 'does not throw an error');
+	t.deepEqual(keysShim(foo), ['a'], 'returns expected keys');
 	t.end();
 });
 
 test('returns names which are own properties', function (t) {
-	var theKeys = keys(obj);
+	var theKeys = keysShim(obj);
 	for (var i = 0; i < theKeys.length; ++i) {
 		t.equal(has.call(obj, theKeys[i]), true, theKeys[i] + ' should be returned');
 	}
@@ -133,7 +133,7 @@ test('returns names which are enumerable', function (t) {
 			loopedValues.push(k);
 		}
 	}
-	var theKeys = keys(obj);
+	var theKeys = keysShim(obj);
 	for (var i = 0; i < theKeys.length; ++i) {
 		t.notEqual(indexOf(loopedValues, theKeys[i]), -1, theKeys[i] + ' is not enumerable');
 	}
@@ -142,7 +142,7 @@ test('returns names which are enumerable', function (t) {
 
 test('throws an error for a non-object', function (t) {
 	t.throws(
-		function () { return keys(42); },
+		function () { return keysShim(42); },
 		new TypeError('Object.keys called on a non-object'),
 		'throws on a non-object'
 	);
@@ -154,7 +154,7 @@ test('works with an object instance', function (t) {
 	Prototype.prototype.foo = true;
 	var instance = new Prototype();
 	instance.bar = true;
-	var theKeys = keys(instance);
+	var theKeys = keysShim(instance);
 	t.equal(is.array(theKeys), true, 'returns an array');
 	t.deepEqual(theKeys, ['bar'], 'Instance has expected keys');
 	t.end();
@@ -164,8 +164,8 @@ test('works in iOS 5 mobile Safari', function (t) {
 	var Foo = function () {};
 	Foo.a = function () {};
 
-	// the bug is keys(Foo) => ['a', 'prototype'] instead of ['a']
-	t.deepEqual(keys(Foo), ['a'], 'has expected keys');
+	// the bug is keysShim(Foo) => ['a', 'prototype'] instead of ['a']
+	t.deepEqual(keysShim(Foo), ['a'], 'has expected keys');
 	t.end();
 });
 
@@ -173,8 +173,8 @@ test('works in environments with the dontEnum bug (IE < 9)', function (t) {
 	var Foo = function () {};
 	Foo.prototype.a = function () {};
 
-	// the bug is keys(Foo.prototype) => ['a', 'constructor'] instead of ['a']
-	t.deepEqual(keys(Foo.prototype), ['a'], 'has expected keys');
+	// the bug is keysShim(Foo.prototype) => ['a', 'constructor'] instead of ['a']
+	t.deepEqual(keysShim(Foo.prototype), ['a'], 'has expected keys');
 	t.end();
 });
 
@@ -194,7 +194,7 @@ test('shadowed properties', function (t) {
 	for (var i = 0; i < shadowedProps.length; ++i) {
 		shadowedObject[shadowedProps[i]] = i;
 	}
-	var shadowedObjectKeys = keys(shadowedObject);
+	var shadowedObjectKeys = keysShim(shadowedObject);
 	shadowedObjectKeys.sort();
 	t.deepEqual(shadowedObjectKeys, shadowedProps, 'troublesome shadowed properties are keys of object literals');
 	t.end();
