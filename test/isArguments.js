ed14c0ac8e8102853d4f7a6e2d0cde01969c5377
Jordan Harband <ljharb@gmail.com>
2018-04-29 22:23:15 -0700

    [Dev Deps] update `eslint`, `nsp`, `tape`

--- test/isArguments.js
+++ test/isArguments.js
@@ -4,16 +4,13 @@ var test = require('tape');
 var isArguments = require('../isArguments');
 
 test('is.arguments', function (t) {
-	t.notOk(isArguments([]), 'array is not arguments', { operator: isArguments });
-	(function () { t.ok(isArguments(arguments), 'arguments is arguments', { operator: isArguments }); }());
-	(function () {
-		var args = Array.prototype.slice.call(arguments);
-		t.notOk(isArguments(args), 'sliced arguments is not arguments', { operator: isArguments });
-	}());
+	t.notOk(isArguments([]), 'array is not arguments');
+	(function () { t.ok(isArguments(arguments), 'arguments is arguments'); }());
+	(function () { t.notOk(isArguments(Array.prototype.slice.call(arguments)), 'sliced arguments is not arguments'); }());
 	var fakeOldArguments = {
 		callee: function () {},
 		length: 3
 	};
-	t.ok(isArguments(fakeOldArguments), 'old-style arguments is arguments', { operator: isArguments });
+	t.ok(isArguments(fakeOldArguments), 'old-style arguments is arguments');
 	t.end();
 });
